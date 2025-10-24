210b41551913d50052eea993e069eb4d26fffada
jonafinmicclen <jonathangmcclen@gmail.com>
2025-10-24 05:14:56 +0100

    Fixed build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
+
+const isBuild = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  ...({
+    site: "https://jonafinmicclen.github.io/voxcel-website",
+    base: "voxcel-website",
+    outDir: "./docs",
+  }),
 });
